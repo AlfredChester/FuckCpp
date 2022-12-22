da4e1eee69dc8caca2fb18d698e8dfe7e4e6f4d0
FredB-mine <fredbao1126@gmail.com>
2022-12-22 08:58:13 +0800

    compresser

--- utils/compress.py
+++ utils/compress.py
@@ -2,7 +2,7 @@ import execjs
 
 _jsFile = open('.\\js\\compress.js', 'r', encoding='u8')
 _jsCompress = execjs.compile(
-    _jsFile.read()
+    _jsFile.read(), cwd='js\\node_modules'
 )
 
 def compress(src: str) -> str:
