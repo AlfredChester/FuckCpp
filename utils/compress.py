ac7ccbbd78c0c7fab2845e7be88d999fd118b55d
FredB-mine <fredbao1126@gmail.com>
2022-12-23 10:46:32 +0800

    Fixed Route Bug

--- utils/compress.py
+++ utils/compress.py
@@ -2,13 +2,15 @@ from utils.constants import *
 from utils.logger import *
 import subprocess as sp
 from sys import platform
+from os import path
+cur_dir = path.dirname(
+     path.dirname(path.abspath(__file__))
+)
 
 @logger.catch
 def compress(src: str) -> str:
-     # cmd = NodeRunTime[platform] + \
-     #     f" .\\js\\compress.js compress --src '{src}'"
      cmd = [ 
-          NodeRunTime[platform], './js/compress.js', 
+          NodeRunTime[platform], f'{cur_dir}/js/compress.js', 
           'compress', '--src', f"'{src}'"
      ]
      ret = sp.check_output(cmd)
