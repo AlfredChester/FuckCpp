a6c46d37d18d0033afb212e94393ffe0e840fd58
FredB-mine <fredbao1126@gmail.com>
2022-12-23 09:18:26 +0800

    Test Confused

--- confused.cpp
+++ confused.cpp
@@ -1,32 +1,32 @@
-#define _0x47b5c7 scanf
-#define _0x321c42 printf
-#define _0xe7f261 cin
-#define _0xa26408 cout
-#define _0x1961c4 putchar
-#define _0x2a8191 puts
-#define _0x7862ee getchar
-#define _0x7676b6 rand
-#define _0x7a983c long long
-#define _0xbca170 int
-#define _0xbfbc05 const
-#define _0x4b6b77 char
-#define _0xc6c548 true
-#define _0x7aff2b false
-#define _0xe38a40 inline
-#define _0x740124 struct
-#define _0xd6c7f9 bool
-#define _0x189995 __gcd
-#define _0x7ad36d memset
-#define _0x41ea93 NULL
-#define _0x7f37d8 private
-#define _0xc00564 public
-#define _0x923466 void
-#define _0xc4ebab while
-#define _0xed1e31 for
-#define _0x1b46fa auto
-#define _0xcb0a86 size_t
-#define _0xbe9935 if
-#define _0xfcf52d return
-#define _0xcbf09a else
+#define _0xcaef93 scanf
+#define _0x370ded printf
+#define _0xa60754 cin
+#define _0x795404 cout
+#define _0xa8fae1 putchar
+#define _0xb98578 puts
+#define _0xe529ee getchar
+#define _0x43d070 rand
+#define _0xae548b long long
+#define _0xad8221 int
+#define _0x652045 const
+#define _0x8c90ca char
+#define _0xacbfa2 true
+#define _0x606e3f false
+#define _0x19239e inline
+#define _0x622eb7 struct
+#define _0xb3721b bool
+#define _0x82d70b __gcd
+#define _0x58a524 memset
+#define _0xfb1a9f NULL
+#define _0xbe0ec9 private
+#define _0x94359e public
+#define _0x753928 void
+#define _0xd0477d while
+#define _0xc44ab1 for
+#define _0x5e21f4 auto
+#define _0x8b0ece size_t
+#define _0x4529c2 if
+#define _0x89a8e9 return
+#define _0xd725a5 else
 #include<iostream>
-using namespace std;_0xbca170 main(_0xbca170 argc,_0xbfbc05 _0x4b6b77*argv[]){_0x321c42("Hello,world");_0xfcf52d 0;}
+using namespace std;_0xad8221 main(_0xad8221 argc,_0x652045 _0x8c90ca*argv[]){_0x370ded("Hello,world");_0x89a8e9 0;}
