2b4f19763dc6661a4077be39a8c473fcafd6c33f
FredB-mine <fredbao1126@gmail.com>
2022-12-24 15:36:49 +0800

    Fixed bugs on linux

--- utils/compress.py
+++ utils/compress.py
@@ -6,15 +6,16 @@ from utils.constants import *
 from utils.logger import *
 
 cur_dir = path.dirname(
-     path.dirname(path.abspath(__file__))
+    path.dirname(path.abspath(__file__))
 )
 
 @logger.catch
 def compress(src: str) -> str:
-     cmd = [ 
-          'lib/' + NodeRunTime[platform], 
-          f'{cur_dir}/js/compress.js', 
-          'compress', '--src', f"'{src}'"
-     ]
-     ret = sp.check_output(cmd)
-     return ret
+    cmd = [
+        'lib/' + NodeRunTime[platform],
+        f'{cur_dir}/js/compress.js',
+        'compress', '--src', f"'{src}'"
+    ]
+    ret = str(sp.check_output(cmd), encoding='u8')
+    logger.debug(f'Compressed Code:\n{ret}')
+    return ret
