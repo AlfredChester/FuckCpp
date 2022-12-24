a6b1390251a6da3d39ed8c2f6193bd9e8832b188
FredB-mine <fredbao1126@gmail.com>
2022-12-24 15:17:19 +0800

    Deleted unused spaces

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from utils.logger import *
 @logger.catch
 def execute(conf) -> None: 
     # Firstly: Mess Codes
-    pData           = conf.ParamData
-    sourceCode      = open(pData['src'], 'r', encoding='u8')
-    sourcelines     = sourceCode.readlines()
-    confusedCode    = ''
+    pData = conf.ParamData
+    sourceCode = open(pData['src'], 'r', encoding='u8')
+    sourcelines = sourceCode.readlines()
+    confusedCode = ''
     for line in sourcelines:
         confusedCode += line
     if not pData['noConfuse']:
-        Confuser        = confuser(pData['level'])
-        confusedCode    = Confuser.genConfused(sourcelines)
+        Confuser = confuser(pData['level'])
+        confusedCode = Confuser.genConfused(sourcelines)
     # Secondly: Zip Codes
     if pData['zipsrc']:
-        confusedCode= compress(confusedCode)
-    outPutFile      = open(pData['output'], 'w', encoding='u8')
+        confusedCode = compress(confusedCode)
+    outPutFile = open(pData['output'], 'w', encoding='u8')
     outPutFile.write(confusedCode)
     outPutFile.close()
     return   
