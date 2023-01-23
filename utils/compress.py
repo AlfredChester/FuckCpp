a19ac73b3a1377cdf0d50b2d3369ff125fb7557b
FredB-mine <fredbao1126@gmail.com>
2023-01-23 12:32:04 +0800

    Changed Arch

--- utils/compress.py
+++ utils/compress.py
@@ -1,13 +1,8 @@
 import subprocess as sp
-from os import path
 from sys import platform
 
 from utils.constants import *
 from utils.logger import *
-
-cur_dir = path.dirname(
-    path.dirname(path.abspath(__file__))
-)
 
 @logger.catch
 def compress(src: str) -> str:
