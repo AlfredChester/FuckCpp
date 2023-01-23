ccc16242c855a16f0deeb1ef3f57ad239419b6c8
FredB-mine <fredbao1126@gmail.com>
2023-01-23 12:32:16 +0800

    Changed Arch

--- utils/logger.py
+++ utils/logger.py
@@ -7,17 +7,13 @@ class MyLogger:
         self.logger = logger
         self.logger.remove()
         self.logger.add(stdout, level = 'INFO',
-            format = '<cyan>{module}.exe</cyan>: '
-                     '<level>{message}</level>'
+            format = consoleLogFormat
         )
         log_level = 'INFO'
         if DEBUG:
             log_level = 'DEBUG'
         self.logger.add(log_name, level = log_level,
-            format = "{time:YYYY.MM.DD HH:mm:ss} - "  # 时间
-                     "{thread.name} | "  # 进程名
-                     "{module}.{function}:{line} - {level}:\n{message}",
-            rotation = '50 MB'
+            format = fileLogFormat, rotation = '50 MB'
         )
 
 
