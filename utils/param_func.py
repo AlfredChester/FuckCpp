5657528e05065b1d8fd5af7297ea85ed6eca57ae
FredB-mine <fredbao1126@gmail.com>
2023-06-05 05:16:26 +0800

    Return functions

--- utils/param_func.py
+++ utils/param_func.py
@@ -2,23 +2,23 @@ from sys import exit
 from time import localtime
 from typing import NoReturn
 
-def genPrintFunc(info : str):
+def genPrintFunc(info : str) -> function:
     def func() -> NoReturn:
         print(info)
         exit(0)
     return func
 
-def genShowHelp(helpInfo : str):
+def genShowHelp(helpInfo : str) -> function:
     return genPrintFunc(helpInfo)
 
-def genShowVersion(version : str):
+def genShowVersion(version : str) -> function:
     return genPrintFunc(
         f'''FuckCpp v{version}
 Copyright (c) 2019-{localtime().tm_year} Dr.Alfred.
 Distributed under the MIT License.'''
     )
 
-def genLogPos(log_dir : str):
+def genLogPos(log_dir : str) -> function:
     return genPrintFunc(
         f'FuckCpp runtime log directory: {log_dir}'
     )
