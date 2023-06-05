2fd58d84a4d5e9cb01de5a668253198f44d8fce8
FredB-mine <fredbao1126@gmail.com>
2023-06-05 21:48:38 +0800

    Removed features that is unstable under Python 3.7

--- utils/param_func.py
+++ utils/param_func.py
@@ -2,23 +2,23 @@ from sys import exit
 from time import localtime
 from typing import NoReturn
 
-def genPrintFunc(info : str) -> function:
+def genPrintFunc(info : str):
     def func() -> NoReturn:
         print(info)
         exit(0)
     return func
 
-def genShowHelp(helpInfo : str) -> function:
+def genShowHelp(helpInfo : str):
     return genPrintFunc(helpInfo)
 
-def genShowVersion(version : str) -> function:
+def genShowVersion(version : str):
     return genPrintFunc(
         f'''FuckCpp v{version}
 Copyright (c) 2019-{localtime().tm_year} Dr.Alfred.
 Distributed under the MIT License.'''
     )
 
-def genLogPos(log_dir : str) -> function:
+def genLogPos(log_dir : str):
     return genPrintFunc(
         f'FuckCpp runtime log directory: {log_dir}'
     )
